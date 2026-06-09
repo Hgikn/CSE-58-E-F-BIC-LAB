58c0798abd71a553033205dac3b092ab10f53d60
Mijan Ahmed <140434556+Hgikn@users.noreply.github.com>
2026-06-09 16:09:13 +0600

    Add functionality to count and print most common patterns
    Implement function to find most common substrings of given length.

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,12 @@
-
+text=input().strip()
+k=int(input())
+count={}
+for i in range(len(text) - k + 1): 
+  pattern=text[i:i+k]
+  count[pattern]=count.get(pattern,0)+1
+max_count=max(count.values()) 
+result=[] 
+for pattern in count: 
+  if count [pattern]==max_count: 
+    result.append(pattern) 
+print(*result)     
